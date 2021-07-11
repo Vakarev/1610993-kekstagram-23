05ac16f49bbbbb89542f7b4cbd54a437a38e2b45
Kirill <je7logic@gmail.com>
2021-07-11 23:57:54 +0300

    module9-task1

---
+++ js/adding-effects.js
@@ -0,0 +1,169 @@
+const newFilePopup = document.querySelector('.img-upload__overlay');
+const effectSlider = document.querySelector('.effect-level__slider');
+const uploudedImageContainer = document.querySelector('.img-upload__preview');
+const uploudedImage = uploudedImageContainer.querySelector('img');
+const effectLevelValue = document.querySelector('.effect-level__value');
+const scaleField = document.querySelector('.scale__control--value');
+const effectRadioList = newFilePopup.querySelectorAll('.effects__radio');
+const scaleControl = newFilePopup.querySelector('.img-upload__scale');
+
+noUiSlider.create(effectSlider, {
+  range: {
+    min: 0,
+    max: 100,
+  },
+  start: 100,
+  step: 1,
+  format: {
+    to: function (value) {
+      if (Number.isInteger(value)) {
+        return value.toFixed(0);
+      }
+      return value.toFixed(1);
+    },
+    from: function (value) {
+      return parseFloat(value);
+    },
+  },
+});
+
+const getSliderOptions = (minValue, maxValue, stepValue) => {
+
+  effectSlider.noUiSlider.updateOptions({
+    range: {
+      min: minValue,
+      max: maxValue,
+    },
+    start: maxValue,
+    step: stepValue,
+    format: {
+      to: function (value) {
+        if (Number.isInteger(value)) {
+          return value.toFixed(0);
+        }
+        return value.toFixed(1);
+      },
+      from: function (value) {
+        return parseFloat(value);
+      },
+    },
+  });
+};
+
+const addingEffects = (evt) => {
+  const targetElementValue  = evt.target.value;
+  let styleFilter;
+
+  if (uploudedImage.classList.length === 2) {
+    uploudedImage.className = 'img-upload__preview';
+  }
+
+  uploudedImage.classList.add(`effects__preview--${targetElementValue}`);
+
+  if(targetElementValue === 'none') {
+    effectSlider.setAttribute('style', 'display: none;');
+    uploudedImage.style.filter = 'none';
+    return;
+  } else {
+    effectSlider.setAttribute('style', 'display: block;');
+  }
+
+  switch (targetElementValue) {
+    case 'chrome':
+      styleFilter = 'grayscale';
+      getSliderOptions(0, 1, 0.1);
+      break;
+    case 'sepia':
+      styleFilter = 'sepia';
+      getSliderOptions(0, 1, 0.1);
+      break;
+    case 'marvin':
+      styleFilter = 'invert';
+      getSliderOptions(0, 100, 1);
+      break;
+    case 'phobos':
+      styleFilter = 'blur';
+      getSliderOptions(0, 3, 0.1);
+      break;
+    case 'heat':
+      styleFilter = 'brightness';
+      getSliderOptions(0, 3, 0.1);
+      break;
+    case 'none':
+      break;
+  }
+
+  effectSlider.noUiSlider.on('update', (_, handle, unencoded) => {
+
+    if(Number.isInteger(unencoded[handle])) {
+      effectLevelValue.value = unencoded[handle].toFixed(0);
+    } else {
+      effectLevelValue.value = unencoded[handle].toFixed(1);
+    }
+
+    if (targetElementValue === 'marvin') {
+      return uploudedImage.style.filter = `${styleFilter}(${effectLevelValue.value}%)`;
+    } else if (targetElementValue === 'phobos') {
+      return uploudedImage.style.filter = `${styleFilter}(${effectLevelValue.value}px)`;
+    }
+    uploudedImage.style.filter = `${styleFilter}(${effectLevelValue.value})`;
+  });
+};
+
+const imageScale = (evt) => {
+
+  const buttonSmaller = document.querySelector('.scale__control--smaller');
+  const buttonBigger = document.querySelector('.scale__control--bigger');
+
+  const reducedImage = () => {
+
+    switch (scaleField.value) {
+      case '100%':
+        uploudedImage.style.transform = 'scale(0.75)';
+        buttonBigger.removeAttribute('disabled', 'disabled');
+        scaleField.value = '75%';
+        break;
+      case '75%':
+        uploudedImage.style.transform = 'scale(0.5)';
+        scaleField.value = '50%';
+        break;
+      case '50%':
+        uploudedImage.style.transform = 'scale(0.25)';
+        buttonSmaller.setAttribute('disabled', 'disabled');
+        scaleField.value = '25%';
+        break;
+    }
+
+  };
+
+  const largeImage = () => {
+
+    switch (scaleField.value) {
+      case '75%':
+        uploudedImage.style.transform = 'scale(1)';
+        buttonBigger.setAttribute('disabled', 'disabled');
+        scaleField.value = '100%';
+        break;
+      case '50%':
+        uploudedImage.style.transform = 'scale(0.75)';
+        scaleField.value = '75%';
+        break;
+      case '25%':
+        uploudedImage.style.transform = 'scale(0.5)';
+        buttonSmaller.removeAttribute('disabled');
+        scaleField.value = '50%';
+        break;
+    }
+  };
+
+  if(evt.target.textContent === 'Уменьшить') {
+    return reducedImage();
+  }
+  largeImage();
+};
+
+scaleControl.addEventListener('click', imageScale);
+
+for(let counter = 0 ; counter <= effectRadioList.length - 1 ; counter++) {
+  effectRadioList[counter].addEventListener('click', addingEffects);
+}
--- js/main.js
+++ js/main.js
@@ -2,3 +2,4 @@ import './show-images.js';
 import './upload-image.js';
 import './form-comments-validation.js';
 import './form-hashtags-validation.js';
+import './adding-effects.js';
